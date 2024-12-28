27c3d66022cea55821111cb2cb59f111469b6689
sofiu <torressofia089@gmail.com>
2024-12-27 23:02:50 -0300

    Agrego Mann-Whitney U-Test

--- src/main/java/rotacionCultivos/ParetoEstimate.java
+++ src/main/java/rotacionCultivos/ParetoEstimate.java
@@ -2,6 +2,7 @@ package rotacionCultivos;
 
 import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 import org.apache.commons.math3.stat.inference.KolmogorovSmirnovTest;
+import org.apache.commons.math3.stat.inference.MannWhitneyUTest;  
 import org.uma.jmetal.algorithm.Algorithm;
 import org.uma.jmetal.operator.crossover.CrossoverOperator;
 import org.uma.jmetal.operator.mutation.MutationOperator;
@@ -20,62 +21,62 @@ import java.util.List;
 public class ParetoEstimate {
 
     public static void main(String[] args) {
-    	List<String> instancias = new ArrayList<>();
-		instancias.add("instanciaChica.xml");
-		instancias.add("instanciaMediana.xml");
-		instancias.add("instanciaGrande.xml");	
+        List<String> instancias = new ArrayList<>();
+        instancias.add("instanciaChica.xml");
+        instancias.add("instanciaMediana.xml");
+        instancias.add("instanciaGrande.xml");  
 
-		List<List<IntegerSolution>> solucionesPorInstancia = new ArrayList<>();
-		List<String> nombresInstancias = List.of("instanciaChica", "instanciaMediana", "instanciaGrande", "instanciaVariada");
-		
-		for (String archivoInstancia : instancias) {
-			
-			String directorioInstancias = "src/main/resources/instancias/";
-			String xmlFilePath = directorioInstancias + archivoInstancia;
-			AgriculturalData data = Main.readDataFromXML(xmlFilePath);
+        List<List<IntegerSolution>> solucionesPorInstancia = new ArrayList<>();
+        List<String> nombresInstancias = List.of("instanciaChica", "instanciaMediana", "instanciaGrande", "instanciaVariada");
 
-			// Crear la instancia del problema
-			AgriculturalOptimizationProblem problem = new AgriculturalOptimizationProblem(data.cantParcelas,
-					data.cantFilas, data.cantSemestres, data.cantCultivos, data.areaParcelas,
-					data.rendimientoCultivoChico, data.rendimientoCultivoMediano, data.rendimientoCultivoGrande,
-					data.precioCultivo, data.costoMantCultivo, data.temporadaCultivo);
-			
-			List<IntegerSolution> solucionesDeInstancia = new ArrayList<>();
+        for (String archivoInstancia : instancias) {
+            
+            String directorioInstancias = "src/main/resources/instancias/";
+            String xmlFilePath = directorioInstancias + archivoInstancia;
+            AgriculturalData data = Main.readDataFromXML(xmlFilePath);
+
+            // Crear la instancia del problema
+            AgriculturalOptimizationProblem problem = new AgriculturalOptimizationProblem(data.cantParcelas,
+                    data.cantFilas, data.cantSemestres, data.cantCultivos, data.areaParcelas,
+                    data.rendimientoCultivoChico, data.rendimientoCultivoMediano, data.rendimientoCultivoGrande,
+                    data.precioCultivo, data.costoMantCultivo, data.temporadaCultivo);
+            
+            List<IntegerSolution> solucionesDeInstancia = new ArrayList<>();
             List<Double> ganancias = new ArrayList<>();
             List<Double> diversidades= new ArrayList<>();
-	
-			for (int i = 0; i<30; i++) {
-				// Medir tiempo total del algoritmo evolutivo NSGA-II
-				long startNSGAII = System.nanoTime();
-	
-				double crossoverProbability = 0.9;
-				double mutationProbability = 1.0 / problem.getNumberOfVariables();
-				double distributionIndex = 10.0;
-	
-				CrossoverOperator<IntegerSolution> crossover = new IntegerSBXCrossover(crossoverProbability,
-						distributionIndex);
-				MutationOperator<IntegerSolution> mutation = new SeasonalIntegerMutation(mutationProbability,
-						data.cantParcelas, data.cantFilas, data.temporadaCultivo);
-	
-				Algorithm<List<IntegerSolution>> algorithm = new NSGAIIBuilder<>(problem, crossover, mutation, 200 // Tamaño  de la población
-				).setMaxEvaluations(50000).setSolutionListEvaluator(new SequentialSolutionListEvaluator<>()).build();
-	
-				algorithm.run();
-				long endNSGAII = System.nanoTime();
-	
-				List<IntegerSolution> population = algorithm.getResult();
-				solucionesDeInstancia.addAll(population);
-				
-				for (IntegerSolution solution : population) {
+
+            for (int i = 0; i<30; i++) {
+                // Medir tiempo total del algoritmo evolutivo NSGA-II
+                long startNSGAII = System.nanoTime();
+
+                double crossoverProbability = 0.9;
+                double mutationProbability = 1.0 / problem.getNumberOfVariables();
+                double distributionIndex = 10.0;
+
+                CrossoverOperator<IntegerSolution> crossover = new IntegerSBXCrossover(crossoverProbability,
+                        distributionIndex);
+                MutationOperator<IntegerSolution> mutation = new SeasonalIntegerMutation(mutationProbability,
+                        data.cantParcelas, data.cantFilas, data.temporadaCultivo);
+
+                Algorithm<List<IntegerSolution>> algorithm = new NSGAIIBuilder<>(problem, crossover, mutation, 200 // Tamaño  de la población
+                ).setMaxEvaluations(50000).setSolutionListEvaluator(new SequentialSolutionListEvaluator<>()).build();
+
+                algorithm.run();
+                long endNSGAII = System.nanoTime();
+
+                List<IntegerSolution> population = algorithm.getResult();
+                solucionesDeInstancia.addAll(population);
+                
+                for (IntegerSolution solution : population) {
                     ganancias.add(solution.getObjective(0));
                     diversidades.add(solution.getObjective(1));
                 }
-			}
-			solucionesPorInstancia.add(obtenerNoDominadas(solucionesDeInstancia));
+            }
+            solucionesPorInstancia.add(obtenerNoDominadas(solucionesDeInstancia));
             calcularEstadisticas(ganancias, nombresInstancias.get(instancias.indexOf(archivoInstancia)), "Ganancia");
             calcularEstadisticas(diversidades, nombresInstancias.get(instancias.indexOf(archivoInstancia)), "Diversidad");
-		}
-		ScatterPlot.guardarFrentePareto(solucionesPorInstancia, nombresInstancias);
+        }
+        ScatterPlot.guardarFrentePareto(solucionesPorInstancia, nombresInstancias);
     }
 
     private static void calcularEstadisticas(List<Double> fitnesses, String nombreInstancia, String objetivo) {
@@ -92,15 +93,19 @@ public class ParetoEstimate {
         System.out.println("Fitness Promedio: " + promedioFitness);
         System.out.println("Desviación Estándar: " + desviacionEstandar);
 
-        // Comparación de distribuciones con Mann-Whitney U-Test (ejemplo)
+        // Comparación de distribuciones con Kolmogorov-Smirnov Test
         if (fitnesses.size() > 1) {
             double[] distribucion1 = fitnesses.subList(0, fitnesses.size() / 2).stream().mapToDouble(Double::doubleValue).toArray();
             double[] distribucion2 = fitnesses.subList(fitnesses.size() / 2, fitnesses.size()).stream().mapToDouble(Double::doubleValue).toArray();
 
             KolmogorovSmirnovTest ksTest = new KolmogorovSmirnovTest();
-            double pValue = ksTest.kolmogorovSmirnovTest(distribucion1, distribucion2);
+            double pValueKS = ksTest.kolmogorovSmirnovTest(distribucion1, distribucion2);
+            System.out.println("p-value (Kolmogorov-Smirnov Test): " + pValueKS);
 
-            System.out.println("p-value (Kolmogorov-Smirnov Test): " + pValue);
+            // Comparación con Mann-Whitney U-Test
+            MannWhitneyUTest mannWhitneyUTest = new MannWhitneyUTest();
+            double pValueMW = mannWhitneyUTest.mannWhitneyUTest(distribucion1, distribucion2);
+            System.out.println("p-value (Mann-Whitney U-Test): " + pValueMW);
         }
         System.out.println();
     }
