b1ca1b0db6a1aeebdf6b967a9643f0d2a97f8a27
Luciano Carrasco Eguren <146464079+lucianoCE@users.noreply.github.com>
2025-08-22 15:28:59 -0300

    fix labels

--- src/main/java/rotacionCultivos/PlotParetoFromCSV.java
+++ src/main/java/rotacionCultivos/PlotParetoFromCSV.java
@@ -86,20 +86,20 @@ public class PlotParetoFromCSV {
 
 
             double[][] puntos = leerCSV(inputFile);
-            dataset.addSeries("Solutions", puntos);
+            dataset.addSeries("MOEA", puntos);
 
             // Add diversity greedy solution
             double[][] diversityPoint = new double[2][1];
             diversityPoint[0][0] = -greedyDiversityResult.get(0).getObjective(0); // Profit
             diversityPoint[1][0] = -greedyDiversityResult.get(0).getObjective(1); // Diversity
-            dataset.addSeries("Greedy Diversity", diversityPoint);
+            dataset.addSeries("Greedy-diversity", diversityPoint);
             System.out.println("Greedy-Diversity point: (" + diversityPoint[0][0] + ", " + diversityPoint[1][0] + ")");
 
             // Add profit greedy solution
             double[][] profitPoint = new double[2][1];
             profitPoint[0][0] = -greedyProfitResult.get(0).getObjective(0);
             profitPoint[1][0] = -greedyProfitResult.get(0).getObjective(1);
-            dataset.addSeries("Greedy Profit", profitPoint);
+            dataset.addSeries("Greedy-profit", profitPoint);
             System.out.println("Greedy-Profit point: (" + profitPoint[0][0] + ", " + profitPoint[1][0] + ")");
 
 
